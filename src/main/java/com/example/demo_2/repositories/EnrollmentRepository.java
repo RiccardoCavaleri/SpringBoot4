f014ec7388c02c94fefa935c028c3d879194c0a3
Riccardo <ric.cavaleri@gmail.com>
2024-09-05 16:04:03 +0200

    Esercizio Database

--- src/main/java/com/example/demo_2/repositories/EnrollmentRepository.java
+++ src/main/java/com/example/demo_2/repositories/EnrollmentRepository.java
@@ -1,4 +1,4 @@
-package repositories;
+package com.example.demo_2.repositories;
 
 import com.example.demo_2.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/example/demo_2/repositories/StudentRepositoy.java
+++ src/main/java/com/example/demo_2/repositories/StudentRepositoy.java
@@ -1,4 +1,4 @@
-package repositories;
+package com.example.demo_2.repositories;
 
 import com.example.demo_2.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
